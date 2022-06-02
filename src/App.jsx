108a128d26868a16c38661086e1836860b8c5b14
KaplenkovAndrew <kaplenkov.andrew@gmail.com>
2022-06-02 16:36:47 +0500

    lesson 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,34 @@ import { Form } from './components/Form/Form';
 import { author as AUTHOR } from './constants';
 
 export const App = () => {
-	const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([]);
 
-	const addMessage = (newMessage) => {
-		setMessages([...messages, newMessage]);
-	};
+  const addMessage = (newMessage) => {
+    setMessages([...messages, newMessage]);
+  };
 
-	useEffect(() => {
-		if (
-			messages.length &&
-			messages[messages.length - 1].author === AUTHOR.user
-		) {
-			const timeOut = setTimeout(() => {
-				addMessage({
-					author: AUTHOR.bot,
-					text: 'response from BOT',
-				});
-			}, 1000);
+  useEffect(() => {
+    if (
+      messages.length &&
+      messages[messages.length - 1].author === AUTHOR.user
+    ) {
+      const timeOut = setTimeout(() => {
+        addMessage({
+          author: AUTHOR.bot,
+          text: 'response from BOT',
+        });
+      }, 1000);
 
-			return () => {
-				clearTimeout(timeOut);
-			};
-		}
-	}, [messages]);
+      return () => {
+        clearTimeout(timeOut);
+      };
+    }
+  }, [messages]);
 
-	return (
-		<>
-			<Message messages={messages} />
-			<Form addMessage={addMessage} />
-		</>
-	);
+  return (
+    <>
+      <Message messages={messages} />
+      <Form addMessage={addMessage} />
+    </>
+  );
 };
